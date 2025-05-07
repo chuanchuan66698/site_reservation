647f7ec5853be295197b9baeb33102d090d8c33c
Chuan <2832419979@qq.com>
2025-05-07 15:00:05 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+# 图书馆预约系统配置
+BASE_URL = "https://iclib.xju.edu.cn"
+
+# 预约相关配置
+RESERVATION_URL = f"{BASE_URL}/#/ic/researchSpace/1/4/4"
+
+# 座位选择配置
+PREFERRED_SEATS = []  # 在这里添加偏好的座位号
+PREFERRED_TIME_SLOTS = []  # 在这里添加偏好的时间段
+
+# 登录配置
+USERNAME = ""  # 学号
+PASSWORD = ""  # 密码
+
+# 预约时间配置
+RESERVATION_START_TIME = "00:00:00"  # 预约开始时间 
